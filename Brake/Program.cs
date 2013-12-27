31d0aaf0c7c599cc6cf7fa97c451c5a4e9ae0763
ttwj <terencetanwj@gmail.com>
2013-12-28 00:17:59 +0800

    0.0.4 release

--- Brake/Program.cs
+++ Brake/Program.cs
@@ -10,8 +10,7 @@ namespace Brake
 {
 	public class MainClass
 	{
-        public const int DEBUG = 1;
-
+		public const int DEBUG = 1;
 
 		public enum Platform
 		{
@@ -45,45 +44,52 @@ namespace Brake
 		[STAThread]
 		public static void Main (string[] args)
 		{
-			String location;
+			Console.WriteLine ("Welcome to Brake!");
+			Console.WriteLine ("Current version: Brake-0.0.4");
+			Container xml = Container.getContainer ();
+			if (xml.Config.host == null) {
+				xml.Config = new Configuration ();
+				Console.Write ("IP address of your iDevice: ");
+				xml.Config.host = Console.ReadLine ();
+				Console.Write ("SSH Port: ");
+				string portString = "22";
+				portString = Console.ReadLine ();
+				int.TryParse (portString, out xml.Config.port);
+				Console.Write ("Root Password: ");
+				xml.Config.Password = Console.ReadLine ();
+				Console.WriteLine ("== Is this correct? Y/N ==");
+				Console.WriteLine ("Host: " + xml.Config.host);
+				Console.WriteLine ("Root Password: " + xml.Config.Password);
+				string confirm = Console.ReadLine ();
+				if (confirm != "y") {
+					return;
+				}
+				xml.SaveXML ();
+			}
 			AppHelper appHelper = new AppHelper ();
-			Console.WriteLine ("IP address of your iDevice: ");
-			string host = Console.ReadLine();
-			string portString = "22";
-			int port;
-			int.TryParse (portString, out port);
-            //COMING SOON PORT VERIFICATION
-            //var ping = new Ping();
-            //var reply = ping.Send(host); // 1 minute time out (in ms)
-            //if (reply.Status == IPStatus.Success)
-            //{
-            //    Console.WriteLine("IP Address Valid");
-            //}
-            //else
-            //{
-            //    Console.WriteLine("Unable to SSH to IP");
-            //}
-			string user = "root";
-			Console.WriteLine ("Root Password: ");
-			string pass = Console.ReadLine ();
-            Console.WriteLine("Is this correct? Y/N");
-            Console.WriteLine("Host:" + host);
-            Console.WriteLine("Root Password:" + pass);
-            string confirm = Console.ReadLine();
-            if (confirm != "y")
-            {
-                return;
-            }
+
+			//COMING SOON PORT VERIFICATION
+			//var ping = new Ping();
+			//var reply = ping.Send(host); // 1 minute time out (in ms)
+			//if (reply.Status == IPStatus.Success)
+			//{
+			//    Console.WriteLine("IP Address Valid");
+			//}
+			//else
+			//{
+			//    Console.WriteLine("Unable to SSH to IP");
+			//}
+	
 			Console.WriteLine ("Establishing SSH connection");
-			var connectionInfo = new PasswordConnectionInfo (host, port, user, pass);
+			var connectionInfo = new PasswordConnectionInfo (xml.Config.host, xml.Config.port, "root", xml.Config.Password);
 			using (var sftp = new SftpClient(connectionInfo)) {
 				using (var ssh = new SshClient(connectionInfo)) {
-                    ssh.Connect ();
+					ssh.Connect ();
 					sftp.Connect ();
 					var whoami = ssh.RunCommand ("Clutch -b");
 					long b;
 					long.TryParse (whoami.Result, out b);
-                    if (b != 13104)
+					/*if (b < 13104)
                     {
                         Console.WriteLine("You're using an old version of Clutch, please update to 1.3.1!");
                         //COMING SOON download Clutch to device for you
@@ -109,10 +115,11 @@ namespace Brake
                         //{
                             return;
                         //}
-                    }
+                    }*/
 					Console.WriteLine ("reply: " + whoami.Result);
 			
 					//return;
+					string location;
 					switch (RunningPlatform ()) {
 					case Platform.Mac:
 						{
@@ -131,53 +138,55 @@ namespace Brake
 							return;
 						}
 					}
-					Container IPAs = appHelper.getIPAs (location);
+					appHelper.getIPAs (location);
 					int i = 1;
 					int a;
-					foreach (IPAInfo ipaInfo in IPAs.Items) {
-						Console.WriteLine (i + ". >> " + ipaInfo.AppName + " (" + ipaInfo.AppVersion + ")");
-						i++;
-					} 
-					Console.WriteLine ("");
-					Console.Write ("Please enter your selection:  ");
-					if (int.TryParse (Console.ReadLine (), out a)) {
-						try {
-							IPAInfo ipaInfo = IPAs.Items [a - 1];
-							Console.WriteLine ("Cracking " + ipaInfo.AppName);
-							String ipalocation = appHelper.extractIPA (ipaInfo);
+					while (true) {
+						foreach (IPAInfo ipaInfo in xml.IPAItems) {
+							Console.WriteLine (i + ". >> " + ipaInfo.AppName + " (" + ipaInfo.AppVersion + ")");
+							i++;
+						} 
+						Console.WriteLine ("");
+						Console.Write ("Please enter your selection:  ");
+						if (int.TryParse (Console.ReadLine (), out a)) {
+							try {
+								IPAInfo ipaInfo = xml.IPAItems [a - 1];
+								Console.WriteLine ("Cracking " + ipaInfo.AppName);
+								String ipalocation = appHelper.extractIPA (ipaInfo);
 
 
-							using (var file = File.OpenRead(ipalocation)) {
-								Console.WriteLine ("Uploading IPA to device..");
-								sftp.UploadFile (file, "Upload.ipa");
+								using (var file = File.OpenRead(ipalocation)) {
+									Console.WriteLine ("Uploading IPA to device..");
+									sftp.UploadFile (file, "Upload.ipa");
 
-							}
+								}
 						
-							Console.WriteLine ("Cracking!!");
-							String binaryLocation = ipaInfo.BinaryLocation.Replace ("Payload/", "");
-							String TempDownloadBinary = Path.Combine (AppHelper.GetTemporaryDirectory (), "crackedBinary");
-							var crack = ssh.RunCommand ("Clutch -i 'Upload.ipa' " + binaryLocation + " /tmp/crackedBinary");
-							Console.WriteLine ("Clutch -i 'Upload.ipa' " + binaryLocation + " /tmp/crackedBinary");
-							Console.WriteLine ("cracking output: " + crack.Result);
+								Console.WriteLine ("Cracking! (This might take a while)");
+								String binaryLocation = ipaInfo.BinaryLocation.Replace ("Payload/", "");
+								String TempDownloadBinary = Path.Combine (AppHelper.GetTemporaryDirectory (), "crackedBinary");
+								var crack = ssh.RunCommand ("Clutch -i 'Upload.ipa' " + binaryLocation + " /tmp/crackedBinary");
+								Console.WriteLine ("Clutch -i 'Upload.ipa' " + binaryLocation + " /tmp/crackedBinary");
+								Console.WriteLine ("cracking output: " + crack.Result);
 						
-							using (var file = File.OpenWrite(TempDownloadBinary)) {
-								Console.WriteLine("Downloading cracked binary..");
-								sftp.DownloadFile ("/tmp/crackedBinary", file);
-							}
+								using (var file = File.OpenWrite(TempDownloadBinary)) {
+									Console.WriteLine ("Downloading cracked binary..");
+									sftp.DownloadFile ("/tmp/crackedBinary", file);
+								}
 
-							String repack = appHelper.repack (ipaInfo, TempDownloadBinary);
-							Console.WriteLine ("Cracking completed, file at " + repack);
-						} catch (IndexOutOfRangeException) {
-							Console.WriteLine ("Invalid input, out of range");
+								String repack = appHelper.repack (ipaInfo, TempDownloadBinary);
+								Console.WriteLine ("Cracking completed, file at " + repack);
+							} catch (IndexOutOfRangeException) {
+								Console.WriteLine ("Invalid input, out of range");
+								return;
+							}
+						} else {
+							Console.WriteLine ("Invalid input");
 							return;
 						}
-					} else {
-						Console.WriteLine ("Invalid input");
-						return;
-					}
 
-					AppHelper.DeleteDirectory (AppHelper.GetTemporaryDirectory ());
-					sftp.Disconnect ();
+						AppHelper.DeleteDirectory (AppHelper.GetTemporaryDirectory ());
+						sftp.Disconnect ();
+					}
 				}
 			}
 		}
